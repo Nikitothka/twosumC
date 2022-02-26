ba856394456662afd8a53f2ee5cd686bd1040c54
NIKITA <nik2003200314@mail.ru>
2022-02-26 19:43:19 +0300

    end

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int twosum() {
 	int i = 0;
-	int b = 0;
+	int b = 1;
 	int j = b;
 	int target = 5;
 	int nums[3] = { 1,2,3 };
-	int n = 2;
-    int size = 3
+    int size = 3;
 
-	while ((nums[i] + nums[j] != target) || (i <=size) || (j<=size)) {
-		while (nums[i] + nums[j] != target) {
+	while ((nums[i] + nums[j] != target) && (i <size) && (j<size)) {
+		while ((nums[i] + nums[j] != target) && (i <size) && (j<size)) {
 			j+=1;
+
 		}
 		i+=1;
 		b+=1;
 		j = b;
 	}
-	int res[2] = { i, j };
-	printf("%d", res[0]);
-	printf("%d", res[1]);
+	int res[2] = { nums[i], nums[j] };
+	printf("%d %d" , res[0], res[1]);
+	//printf("%d", nums[j]);
 
 }
 
